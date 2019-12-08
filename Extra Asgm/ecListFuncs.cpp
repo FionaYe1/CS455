b78cad3ac5607e088d84a9415cacd45e46158d36
yeshuna <yeshn@shanghaitech.edu.cn>
2019-12-08 01:54:49 -0800

    all completed(except comments)

--- Extra Asgm/ecListFuncs.cpp
+++ Extra Asgm/ecListFuncs.cpp
@@ -47,16 +47,100 @@ void longestRun(ListType list, int & maxRunVal, int & maxRunLen) {
 
 void removeMultiplesOf3(ListType & list) {
 
+   ListType newNode = new Node(INT16_MIN);   // create a dummy node
+   newNode->next = list;
+   list = newNode;
+   ListType p = list;
+   ListType temp = list;
+
+   while (p != NULL)
+   {
+      if (p->data % 3 == 0)
+      {
+         temp->next = p->next;
+         delete p;
+         p = temp->next;
+         continue;
+      }
+      temp = p;
+      p = p->next;
+   }
+   list = newNode->next;
+   delete newNode;
 }
 
 
 void insertMiddle(ListType & list, int midVal) {
-   
+   ListType midNode = new Node(midVal);   
+   if (list == NULL || list->next == NULL)
+   {
+      midNode->next = list;
+      list = midNode;
+      return;
+   }
+   // calculate the length of the list
+   ListType p = list;
+   int len = 0;
+   while (p != NULL)
+   {
+      len++;
+      p = p->next;
+   }
+   int num = 0;
+   p = list;
+   // insert the midVal in the position of len/2
+   while (num != len/2 - 1)
+   {
+      num++;
+      p = p->next;
+   }
+   ListType temp = p->next;
+   p->next = midNode;
+   midNode->next = temp;
 }
 
 
 ListType merge(ListType list1, ListType list2) {
-   return NULL;  // dummy code so starter code compiles
+   ListType p1 = list1;
+   ListType p2 = list2;
+   ListType temp = list2;
+   ListType newGuy = new Node(INT16_MIN);    // create a dummy node
+   ListType resultList = newGuy;
+
+   while(p1 != NULL && p2 != NULL)
+   {
+      if (p1->data > p2->data)
+      {
+         newGuy->next = p2;
+         newGuy = newGuy->next;
+         p2 = p2->next;
+      }
+      else if (p1->data < p2->data)
+      {
+         newGuy->next = p1;
+         newGuy = newGuy->next;
+         p1 = p1->next;
+      }
+      else if (p1->data == p2->data)
+      {
+         newGuy->next = p1;
+         newGuy = newGuy->next;
+         p1 = p1->next;
+         temp = p2->next;
+         delete p2;
+         p2 = temp;
+      }
+   }
+   if (p1 == NULL)
+   {
+      newGuy->next = p2;
+   }
+   else
+   {
+      newGuy->next = p1;
+   }
+   resultList = resultList->next;
+   return resultList;  // dummy code so starter code compiles
 }
 
 
